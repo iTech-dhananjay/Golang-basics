15fc2e08ff70b8d3f8146eaddefb9b0103437fdd
Dhananjay Kumar <officialdhananjay76@gmail.com>
2023-12-19 15:47:22 +0530

    Add varaible in go

--- hello.go
+++ hello.go
@@ -3,6 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World! Welcome to Go")
-
+	fmt.Println("Hello, World!")
 }
--- variables.go
+++ variables.go
@@ -1,6 +1,12 @@
 package main
 
+import "fmt"
+
 func main() {
 	var message string
+	message = "Hello, Go!"
 
+	age := 25 // short declaration
+
+	fmt.Println(message, "I am", age, "years old.")
 }
